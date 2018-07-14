944a934933337edcd5ed79053838abf5d88018d3
Dmitriy Shekhovtsov <valorkin@gmail.com>
2018-07-14 14:15:34 +0300

    feat(nest): upgrade to nestjs v5 and upgraded auth0 strategy to latest version

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,47 @@
-import { Module, MiddlewaresConsumer, RequestMethod } from '@nestjs/common';
-import { EnsureLoggedIn } from './app.middleware';
-import { AppController } from './app.controller';
-import { Auth0Strategy } from './auth0.strategy';
-import { authenticate } from 'passport';
+import {Injectable, MiddlewareConsumer, Module, NestMiddleware, NestModule, RequestMethod} from '@nestjs/common';
+import {EnsureLoggedIn} from './app.middleware';
+import {AppController} from './app.controller';
+import {Auth0Strategy} from './auth0.strategy';
+import {authenticate} from 'passport';
+
+@Injectable()
+class Auth0LoginMiddleware implements NestMiddleware {
+  resolve() {
+    return authenticate('auth0', {
+      clientID: process.env.AUTH0_CLIENT_ID,
+      domain: process.env.AUTH0_DOMAIN,
+      redirectUri: process.env.AUTH0_CALLBACK_URL,
+      audience: 'https://' + process.env.AUTH0_DOMAIN + '/userinfo',
+      responseType: 'code',
+      scope: 'openid profile'
+    });
+  }
+}
+
+@Injectable()
+class Auth0CallbackMiddleware implements NestMiddleware {
+  resolve() {
+    return authenticate('auth0', {
+      successRedirect: '/user',
+      failureRedirect: '/'
+    });
+  }
+}
 
 @Module({
-  components: [ Auth0Strategy ],
+  providers: [Auth0Strategy, Auth0LoginMiddleware, Auth0CallbackMiddleware],
   controllers: [AppController]
 })
-
-export class ApplicationModule {
-
-  configure(consumer: MiddlewaresConsumer) {
+export class ApplicationModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
     consumer
+      .apply(Auth0LoginMiddleware)
+      .forRoutes({path: '/login', method: RequestMethod.ALL})
+
+      .apply(Auth0CallbackMiddleware)
+      .forRoutes({path: '/callback', method: RequestMethod.ALL})
+
       .apply(EnsureLoggedIn)
-      .forRoutes({ path: 'user', method: RequestMethod.ALL })
-      .apply(authenticate('auth0', {
-        successRedirect: '/user',
-        failureRedirect: '/'
-      }))
-      .forRoutes({ path: 'callback',method: RequestMethod.ALL})
+      .forRoutes({path: '/user', method: RequestMethod.ALL});
   }
 }
